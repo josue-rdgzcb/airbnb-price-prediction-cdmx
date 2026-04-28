7a9c702d54ba5942004fe2469833856f71977908
josue-rdgzcb <josue.rdgzcb@gmail.com>
2026-04-27 23:12:21 -0600

    add attractions density function

--- src/features/build_features.py
+++ src/features/build_features.py
@@ -1,13 +1,34 @@
-
 from src.features.geo import compute_min_distance_balltree
-from src.settings.settings import ATTRACTION_POINTS
+from functools import lru_cache
+from src.data.external_data import get_pois_attractions
+from src.features.geo import compute_points_within_radius
 
 
+@lru_cache(maxsize=1)
+def load_attraction_points():
+    return get_pois_attractions()
+
+# =============
 def add_distance_to_attractions(df):
+
+    pois_attractions = load_attraction_points()
 
     df["dist_to_nearest_attraction"] = compute_min_distance_balltree(
         df[["latitude", "longitude"]],
-        ATTRACTION_POINTS
+        pois_attractions
+    )
+
+    return df
+
+# ============
+def add_attractions_density(df):
+
+    pois_attractions = load_attraction_points()
+
+    df["attractions_within_radius"] = compute_points_within_radius(
+        df[["latitude", "longitude"]],
+        pois_attractions,
+        radius_km=1.0
     )
 
     return df
