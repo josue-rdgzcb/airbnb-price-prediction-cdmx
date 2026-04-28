0dd2c54152de16e29d1f8e4ed0dfaf5099ecc9a9
josue-rdgzcb <josue.rdgzcb@gmail.com>
2026-04-27 23:14:32 -0600

    add get_pois_attractions function

--- src/data/external_data.py
+++ src/data/external_data.py
@@ -1,1 +1,13 @@
- 
+from pathlib import Path
+import pandas as pd
+
+BASE_DIR = Path(__file__).resolve().parents[2]
+
+def get_pois_attractions():
+    path = BASE_DIR / "data/external/pois_attractions.csv"
+
+    df = pd.read_csv(path)
+
+    df = df.dropna(subset=["latitude", "longitude"])
+
+    return df[["latitude", "longitude"]].values 
