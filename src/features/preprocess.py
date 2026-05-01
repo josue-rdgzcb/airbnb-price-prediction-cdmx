3344aa7ec68d23e9ff34beb6c2da59208709cb65
josue-rdgzcb <josue.rdgzcb@gmail.com>
2026-04-30 22:42:44 -0600

    add normalize string function to preprocessing module

--- src/features/preprocess.py
+++ src/features/preprocess.py
@@ -1,1 +1,9 @@
  
+# Normalize categorical string values (lowercase + replace spaces with underscores)
+def normalize_string_column(series):
+    """
+    Normalize a pandas Series of strings:
+    - Convert to lowercase
+    - Replace spaces with underscores
+    """
+    return series.str.lower().str.replace(" ", "_")
