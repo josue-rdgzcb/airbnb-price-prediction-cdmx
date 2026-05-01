fa37b7a277e62e0763ca1bb64a1abeaee978e37b
josue-rdgzcb <josue.rdgzcb@gmail.com>
2026-05-01 15:37:22 -0600

    refactor(preprocess): add parse_column function for reuse

--- src/features/preprocess.py
+++ src/features/preprocess.py
@@ -1,4 +1,6 @@
- 
+import pandas as pd 
+import ast
+
 # Normalize categorical string values (lowercase + replace spaces with underscores)
 def normalize_string_column(series):
     """
@@ -7,3 +9,18 @@ def normalize_string_column(series):
     - Replace spaces with underscores
     """
     return series.str.lower().str.replace(" ", "_")
+
+# Parse column into Python lists
+def parse_column(x):
+    """
+    Parse a stringified host verifications value into a Python list.
+    - Return [] if value is NaN or cannot be parsed.
+    """
+    if pd.isna(x):
+        return []
+    
+    try:
+        return ast.literal_eval(x)
+    except (ValueError, SyntaxError):
+        return []
+    
